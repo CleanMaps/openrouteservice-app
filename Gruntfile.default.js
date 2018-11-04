d9e464bb1d2b2199e83f081fbacaded7c4d88774
Joris Clement <joris.clement@posteo.de>
2018-11-05 00:51:38 +0100

    Adapt url to our server.

--- Gruntfile.default.js
+++ Gruntfile.default.js
@@ -297,7 +297,7 @@ module.exports = function(grunt) {
                         name: "production",
                         geocode: "https://api.openrouteservice.org/geocoding",
                         directions:
-                            "https://api.openrouteservice.org/directions",
+                            "http://localhost:8000/directions",
                         isochrones:
                             "https://api.openrouteservice.org/isochrones",
                         matrix: "https://api.openrouteservice.org/matrix",
